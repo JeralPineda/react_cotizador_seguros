754707cf20e8c4ea8ee7cb7678bb7e836042300e
Jeral <jeral.cris@gmail.com>
2021-12-14 15:56:44 -0600

    Creando styled components para los contenedores

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import styled from '@emotion/styled';
+
 import Header from './components/Header';
+
+const Contenedor = styled.div`
+   max-width: 600px;
+   margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+   background-color: #fff;
+   padding: 3rem;
+`;
 
 function App() {
    return (
-      <div>
+      <Contenedor>
          <Header titulo='Cotizador de Seguros' />
-      </div>
+
+         <ContenedorFormulario></ContenedorFormulario>
+      </Contenedor>
    );
 }
 
