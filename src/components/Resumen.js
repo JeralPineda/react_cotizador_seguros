cf486f66bf568f1d94127fcc05011a292e9fef5b
Jeral <jeral.cris@gmail.com>
2021-12-19 22:29:40 -0600

    Finalizano el componente resumen

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,18 +1,30 @@
+import styled from '@emotion/styled';
+
+import { textoCapitalizado } from '../helper';
+
+const ContenedorResumen = styled.div`
+   background-color: #00838f;
+   color: #fff;
+   padding: 1rem;
+   margin-top: 1rem;
+   text-align: center;
+`;
+
 const Resumen = ({ datos }) => {
    const { marca, year, plan } = datos;
 
    if (marca === '' || year === '' || plan === '') return null;
 
    return (
-      <>
+      <ContenedorResumen>
          <h2>Resumen de Cotización</h2>
 
          <ul>
-            <li>Marca: {marca}</li>
-            <li>Plan: {plan}</li>
+            <li>Marca: {textoCapitalizado(marca)}</li>
+            <li>Plan: {textoCapitalizado(plan)}</li>
             <li>Año del auto: {year}</li>
          </ul>
-      </>
+      </ContenedorResumen>
    );
 };
 
